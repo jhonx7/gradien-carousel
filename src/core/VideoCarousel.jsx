598d43274820a5a8cdfc1a713fe9fa6c6b78dbe8
Joni Setiawan <setiawanwhae@gmail.com>
2023-05-26 17:22:28 +0700

    update bg

--- src/core/VideoCarousel.jsx
+++ src/core/VideoCarousel.jsx
@@ -4,29 +4,31 @@ import ReactPlayer from "react-player";
 import { useNavigate } from "react-router-dom";
 
 const VideoCarousel = () => {
-  const [vid,setVid] = useState(0);
+  const [vid, setVid] = useState(0);
   const navigate = useNavigate();
 
   return (
-    <div className='player-wrapper'>
-    <ReactPlayer
-      className='react-player'
-      url={videos[vid]}
-      // pip={true}
-      controls={true}
-      playing={true}
-      width='100%'
-      height='100%'
-      onEnded={()=>{
-        let x = vid + 1;
-        let y = x > (videos.length-1) ? 0 : x;
-        setVid(y)
-        // console.log(x);
-        if (x > (videos.length-1)) {
-          return navigate("/");
-      }
-      }}
-    />
+    <div style={{ padding: "12vh 10vw 0px 10vw" }}>
+      <div>
+        <ReactPlayer
+          
+          url={videos[vid]}
+          // pip={true}
+          controls={true}
+          playing={true}
+          width="80vw"
+          height="80vh"
+          onEnded={() => {
+            let x = vid + 1;
+            let y = x > videos.length - 1 ? 0 : x;
+            setVid(y);
+            // console.log(x);
+            if (x > videos.length - 1) {
+              return navigate("/");
+            }
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/core/WebCarousel.jsx
+++ src/core/WebCarousel.jsx
@@ -6,28 +6,30 @@ import { useNavigate } from "react-router-dom";
 function WebCarousel() {
   const navigate = useNavigate();
   return (
-    <Carousel
-      fade
-      controls={false}
-      onSelect={(item) => {
-        // console.log(item);
-        if (item == 0) {
-           return navigate("/video");
-        }
-      }}
-    >
-      {web.map((item) => (
-        <Carousel.Item key={item} interval={10000}>
-          <iframe
-            style={{ height: "100vh", width: "100vw", overflow: "hidden" }}
-            src={item}
-            // allow='autoplay; encrypted-media'
-            // allowFullScreen
-            // title='video'
-          />
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div style={{padding: " 12vh 5vw 0px 5vw "}}>
+      <Carousel
+        fade
+        controls={false}
+        onSelect={(item) => {
+          // console.log(item);
+          if (item == 0) {
+            return navigate("/video");
+          }
+        }}
+      >
+        {web.map((item) => (
+          <Carousel.Item key={item} interval={10000}>
+            <iframe
+              style={{ height: "80vh", width: "100vw", overflow: "hidden" }}
+              src={item}
+              // allow='autoplay; encrypted-media'
+              // allowFullScreen
+              // title='video'
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 }
 
